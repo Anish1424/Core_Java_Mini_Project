104a62f44fa3ea9aa97083b677fa1e0db6efb231
Anish_dawkhar <147154149+Anish1424@users.noreply.github.com>
2026-08-09 14:36:10 +0530

    Update Billing.java

--- Medical_Store_Management_System/src/com/acc/service/Billing.java
+++ Medical_Store_Management_System/src/com/acc/service/Billing.java
@@ -8,7 +8,7 @@ import com.acc.model.Medical;
 public class Billing {
     private Cart cart = new Cart();
     Scanner scanner = new Scanner(System.in);
-    //int i = 0;
+    //int i;
     
     public void cartMenu( MedicineOperations operations) {
         while (true) {
